8166a96f528df34fedd2fe5076d8e867c08a45dd
heyhadi <hadi.munawirul@gmail.com>
2022-10-17 16:50:15 +0700

    add: pwa

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,36 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte(),  VitePWA({
+    filename: 'sw.ts',
+    includeAssets: ['*.svg'],
+    includeManifestIcons: false,
+    injectRegister: false,
+    manifest: {
+      name: 'Svelte Quran',
+      short_name: 'Svelte Quran',
+      description: 'Read and listen Quran on the go',
+      theme_color: '#ced4da',
+      icons: [
+        {
+          src: 'vite.svg',
+          sizes: '192x192',
+          type: 'image/svg',
+        },
+        {
+          src: 'vite.svg',
+          sizes: '512x512',
+          type: 'image/svg',
+        },
+        {
+          src: 'vite.svg',
+          sizes: '512x512',
+          type: 'image/svg',
+          purpose: 'maskable',
+        },
+      ],
+    },
+    srcDir: 'src/service-worker',
+    strategies: 'injectManifest',
+  })]
 })
