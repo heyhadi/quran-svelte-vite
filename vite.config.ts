1c463fe3096be53785a9823eaa134c9d9538bf65
heyhadi <hadi.munawirul@gmail.com>
2022-10-17 14:21:59 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,26 +4,5 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), VitePWA( {
-    workbox: {
-        globPatterns: ['**/*.{js,css,html,svg,png,woff2}'],
-    },
-    registerType: "autoUpdate",
-    manifest: {
-        "background_color": "#ffffff",
-        "theme_color": "#7E1F86",
-        "name": "Unixfy.net",
-        "short_name": "Unixfy.net",
-        "start_url": "/",
-        "display": "standalone",
-        "icons": [
-            {
-                "src": "/images/largeicon.png",
-                "sizes": "1000x1000",
-                "type": "image/png",
-                "purpose": "maskable any"
-            }
-        ]
-    }
-})]
+  plugins: [svelte()]
 })
