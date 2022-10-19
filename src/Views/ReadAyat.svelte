ddd083a43d7698b6546286d9a147ada7ad6dd185
heyhadi <hadi.munawirul@gmail.com>
2022-10-19 14:14:09 +0700

    Fix: loading psinner

--- src/Views/ReadAyat.svelte
+++ src/Views/ReadAyat.svelte
@@ -1,32 +1,32 @@
 <script lang="ts">
-    import AyatCard from "../components/AyatCard.svelte"
-	import ScrollTop from "../components/ScrollTop.svelte"
-    import { useQuery } from "@sveltestack/svelte-query";
+	import AyatCard from "../components/AyatCard.svelte";
+	import ScrollTop from "../components/ScrollTop.svelte";
+	import { useQuery } from "@sveltestack/svelte-query";
 
-    const surahId:string = window.location.pathname.split("/baca/")[1];
+	const surahId: string = window.location.pathname.split("/baca/")[1];
 
 	const queryResult = useQuery("ayat", () => {
-		return fetch(`https://quranapi.idn.sch.id/surah/${surahId}`).then((res) =>
-			res.json()
+		return fetch(`https://quranapi.idn.sch.id/surah/${surahId}`).then(
+			(res) => res.json()
 		);
 	});
 
-    // $: console.log($queryResult);
-    
+	// $: console.log($queryResult);
 </script>
+
 {#if surahId !== "1"}
-{#if !$queryResult.isFetching}
-<h1 class="text-center">بِسْمِ اللّٰهِ الرَّحْمٰنِ الرَّحِيْمِ</h1>
+	{#if !$queryResult.isFetching}
+		<h1 class="text-center">بِسْمِ اللّٰهِ الرَّحْمٰنِ الرَّحِيْمِ</h1>
+	{/if}
 {/if}
-{/if}
-<AyatCard queryResult = {$queryResult}/>
+<AyatCard queryResult={$queryResult} />
 <ScrollTop />
 
 <style>
-    h1 {
+	h1 {
 		font-weight: bold !important;
 		font-size: 40px;
 		font-family: "Amiri Quran";
-        margin-bottom:60px;
+		margin-bottom: 60px;
 	}
 </style>
